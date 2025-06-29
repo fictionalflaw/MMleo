8d11a29e40b4edbddba7e4227a92713c9b7c5a18
fictionalflaw <2709420461@qq.com>
2025-06-29 20:06:26 +0800

    feat:巡演初步

--- agent/custom/action/__init__.py
+++ agent/custom/action/__init__.py
@@ -2,10 +2,12 @@ from .music_player import *
 from .composite_game import *
 from .composite_game_mini import *
 from .count import *
+from .tour_concert import *
 __all__=[
     "TargetAreaSearchAndSave",
     "MusicPlayer",
     "CompositeGamePlayer",
     "CompositeGamePlayer_mini",
-    "CountTask"
+    "CountTask",
+    "TourConcert"
 ]
--- agent/custom/action/tour_concert.py
+++ agent/custom/action/tour_concert.py
@@ -14,5 +14,32 @@ class   TourConcert(CustomAction):
         context: Context,
         argv: CustomAction.RunArg,
     ) -> CustomAction.RunResult:
-            
+        '''
+        custom_param:{
+        tour_count:int
+        }
+        默认会打完一天的四首歌，不满四首时识别是否回到主页作为中断条件
+        起点：打歌界面第一首，不是也无所谓，反正打完一天或是不满一天都会中断
+        '''
+        degree="Feat_难度调整"
+        tour_count=argv.get("tour_count")
+
+        for i in range(4):
+            logger.info(f"开始第{i+1}次打歌...")
+            context.run_task(degree)
+            context.run_task("Feat_编队调整",
+                {"调整_寻找目标编队":
+                 {
+                    "next":f"for_编队调整_巡演{i+1}"             
+                    }
+                }
+            )
+            if i==3:
+                context.run_task("Feat_巡演BP调整")#interface中修改切10的enable                 
+            else:
+                context.run_task("Feat_巡演BP调整_切3")#interface中修改其中关于BP回复的选项
+            #调完bp，开始打歌了
+            context.run_task("Feat_开始演唱会")
+            out=context.run_task("Flag_TourMap")
+            print("结果:",out)
         return CustomAction.RunResult(success=True)
